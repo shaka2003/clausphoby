070dde83fcb3e702f7c397abb21e19e164f3205f
shaka pratama ichsan <shakapratama2003@gmail.com>
2023-06-10 12:22:49 +0700

    soket kunci level 1

---
+++ escape room V1/Assets/Scripts/Level1ScoreScript.cs
@@ -0,0 +1,21 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class Level1ScoreScript : MonoBehaviour
+{
+    [SerializeField] private int score;
+    [SerializeField] private int finalScore;
+    [SerializeField] private UnityEvent winAction;
+
+    public void updateScore()
+    {
+        score++;
+        if(score == finalScore)
+        {
+            winAction.Invoke();
+        }
+    }
+}
